b1e0a4b3929d5a9afe3658bfb31f6a85760444ce
bhaskar <bhaskar.bharath@gmail.com>
2014-04-03 23:43:17 -0400

    added a file for PCA reduction

---
+++ pca_run.m
@@ -0,0 +1,15 @@
+
+%  Before running PCA, it is important to first normalize X by subtracting 
+%  the mean value from each feature
+% X is mxn
+[X_norm, mu, sigma] = featureNormalize(X);
+
+%  Run PCA
+% U, S are nxn
+[U, S] = pca(X_norm);
+
+%first 100 eigen vectors
+K = 100;
+%Project data
+% X is (mxn)*(nxk) = (mxk) => reduction
+Z = X*U(:, 1:K);
